b7f2095de687a0217095a7c8dcac77638f2aefb8
tdhlakama <tdhlakama@hitrac.co.zw>
2018-10-18 13:44:32 +0200

    Changes to view product

--- src/componets/Main/index.js
+++ src/componets/Main/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Products from '../../containers/Products';
 import Product from '../../containers/Product';
+import ProductScroll from '../../containers/ProductScroll';
+import MostViewedProducts from '../../containers/MostViewedProducts';
+import NearMeProducts from '../../containers/NearMeProducts';
 
 const Main = (props) => (
 
@@ -9,6 +12,10 @@ const Main = (props) => (
     <Route exact path="/" component={Products}></Route>
     <Route exact path="/products" component={Products}></Route>
     <Route exact path="/products/:id" component={Product}></Route>
+    <Route exact path="/productScroll" component={ProductScroll}></Route>
+    <Route exact path="/mostViewed" component={MostViewedProducts}></Route>
+    <Route exact path="/nearMe" component={NearMeProducts}></Route>
+
   </Switch>
 
 );
